21f5f73caaa452d5558f25e835c750556e09bab9
jae kim <jaehkim23@gmail.com>
2023-10-13 18:19:35 -0500

    working on getting user input weather data from api

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,7 @@
-// function getWeather(city){
-//     //let city = $('#weather-input').val()
-//     let queryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=4704c9c7e0563a83601c7f6cd4e3b0a7`
-//     $.ajax({
-//         url: queryUrl,
-//         method: 'GET'
-//     }).then(function(response){
-//         console.log(JSON.stringify(response, null, 2)) 
-//     })
-// }
-// getWeather(Dallas)
-
-
-function getApi(){
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=4704c9c7e0563a83601c7f6cd4e3b0a7'
-    fetch(requestUrl)
-    .then(function response(){
-        return response.json(); 
-    })
-    console.log(getApi)
-}
+ var cityInput = document.querySelector(".weather-input");
+ var searchButton = document.querySelector(".btn");
+ searchButton.addEventListener("click", function(){
+fetch("https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&appid=4704c9c7e0563a83601c7f6cd4e3b0a7")
+    .then(response => response.json())
+    .then(data=> console.log(data))
+ })
