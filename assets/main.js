12bfb281023803d08a4dcccff291aeb363e1194c
jae kim <jaehkim23@gmail.com>
2023-10-14 11:15:59 -0500

    working on adding 5 day weather forecast

--- assets/main.js
+++ assets/main.js
@@ -7,50 +7,38 @@
   var nextTempature = document.querySelector(".nextTemp")
   var nextWind = document.querySelector(".nextWind")
   var nextHumid = document.querySelector(".nextHumidity")
+  var forecastCards = document.querySelector(".forecastcards");
 
   searchButton.addEventListener("click", function(){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=imperial&appid=4704c9c7e0563a83601c7f6cd4e3b0a7`)
-     .then(response => response.json())
-     .then(data=>{
-        var nameValue = data['name'];
-        var tempValue = data['main']['temp'];
-        var windSpeed = data['wind']['speed'];
-        var humidityValue = data['main']['humidity']
+    // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=imperial&appid=4704c9c7e0563a83601c7f6cd4e3b0a7`)
+    //  .then(response => response.json())
+    //  .then(data=>{
+    //     var nameValue = data['name'];
+    //     var tempValue = data['main']['temp'];
+    //     var windSpeed = data['wind']['speed'];
+    //     var humidityValue = data['main']['humidity']
 
-        currentCity.innerHTML = nameValue;
-        currentTempature.innerHTML = "Temp: " + tempValue;
-        currentWind.innerHTML = "Wind: " + windSpeed;
-        currentHumidity.innerHTML ="Humidity: " + humidityValue;
-     })
+    //     currentCity.innerHTML = nameValue;
+    //     currentTempature.innerHTML = "Temp: " + tempValue;
+    //     currentWind.innerHTML = "Wind: " + windSpeed;
+    //     currentHumidity.innerHTML ="Humidity: " + humidityValue;
+    //  })
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}&units=imperial&exclude=minutely,hourly&appid=4704c9c7e0563a83601c7f6cd4e3b0a7`)
     .then(response => response.json())
-    //.then(data=>console.log(data))
+    .then(data=>console.log(data))
     .then(data=>{
         for(i=0; i<5; i++){
-            var nextTemp = data ['list'] [i] ['main'] ['temp'];
-            nextTempature.innerHTML = nextTemp;
+          var cardDiv = document.createElement("div");
+          cardDiv.setAttribute("class", "card");
+          forecastCards.appendChild(cardDiv);
+          var tempatureP = document.createElement("p");
+          tempatureP.setAttribute("class", "nextTemp");
+          cardDiv.appendChild(tempatureP);
+
+             var nextTempature = data ['list'] [i] ['main'] ['temp'];
+             tempatureP.innerHTML = "Temp: " + nextTempature;
        
         }
     })
 })
-
-// for(i=0; i<5; i++){
-//     var nextTemp = data ['list'] ['main'] ['temp'];
-//     var nextWindSpeed = data ['list'] ['wind'] ['speed'];
-//     var nextHumidity = data ['list'] ['main'] ['humidity'];
-
-//     nextTempature.innerHTML = nextTemp;
-//     nextWind.innerHTML = nextWindSpeed;
-//     nextHumid.innerHTML = nextHumidity;
-// }
-
-
-
-
-
-
-
-
-
-  //.then(data=>console.log(data))
